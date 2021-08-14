9f449cb801c2870d7a0e92925a2ddac63a17cd31
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-08-14 14:07:10 +0900

    画面遷移と戻る処理の実装

--- app/src/main/java/com/example/doubleroulette/MainActivity.kt
+++ app/src/main/java/com/example/doubleroulette/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.doubleroulette
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.doubleroulette.databinding.ActivityMainBinding
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -18,7 +20,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.toRouletteButton.setOnClickListener {
-            // TODO: ボタン押下時の処理を追記（ルーレット画面へ遷移）
+            // ルーレット画面へ遷移
+            val intent = Intent(this, RouletteActivity::class.java)
+            // TODO: ここでルーレットセルのデータを渡す（map<string, array<string>>かな？）
+            intent.putExtra("CELL_DATA", "put map data")
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/example/doubleroulette/RouletteActivity.kt
+++ app/src/main/java/com/example/doubleroulette/RouletteActivity.kt
@@ -2,10 +2,22 @@ package com.example.doubleroulette
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.doubleroulette.databinding.ActivityRouletteBinding
 
 class RouletteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRouletteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_roulette)
+
+        binding = ActivityRouletteBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // ルーレットセルデータを取得
+        val cellData = intent.getStringExtra("CELL_DATA")
+        // TODO: セルデータを活用する処理を追記
+
+        binding.backButton.setOnClickListener { finish() }
     }
 }
