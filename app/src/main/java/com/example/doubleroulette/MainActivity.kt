898b14de1ce2172062810e31c334456e097d4b69
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-08-14 09:31:38 +0900

    UIとコードのバインディング設定とボタンの追加

--- app/src/main/java/com/example/doubleroulette/MainActivity.kt
+++ app/src/main/java/com/example/doubleroulette/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.doubleroulette
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.doubleroulette.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.addButton.setOnClickListener {
+            // TODO: ボタン押下時の処理を追記
+        }
+
     }
+
 }
