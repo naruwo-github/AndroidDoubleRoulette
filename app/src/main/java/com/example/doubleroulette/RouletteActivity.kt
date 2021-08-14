054c4c0cd12ae01931c380441d937befb6569b7f
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-08-14 13:33:24 +0900

    ルーレット画面の追加（画面遷移はまだ）

---
+++ app/src/main/java/com/example/doubleroulette/RouletteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.doubleroulette
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RouletteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_roulette)
+    }
+}
