db52ffa2a7af77e2bb886e03acdee019bfd8c4d5
Narumi Nogawa <naruo0711@ab.auone-net.jp>
2021-08-17 19:36:04 +0900

    Realmを使用するための設定を追記

---
+++ app/src/main/java/com/example/doubleroulette/DoubleRouletteApplication.kt
@@ -0,0 +1,19 @@
+package com.example.doubleroulette
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+// Applicationクラスはアクティビティよりも先に呼ばれる
+// このApplicationクラスを継承したクラスのonCreate内で、Realmデータベースの設定処理を行う
+class DoubleRouletteApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val config = RealmConfiguration.Builder()
+            .allowWritesOnUiThread(true).build()
+        Realm.setDefaultConfiguration(config)
+    }
+
+}
