bf77dbf39a8ba623f3bd518671da7c033f5b8b2e
野川成己 <naruo0711@ab.auone-net.jp>
2021-08-29 19:20:24 +0900

    セル描画処理とラベル描画処理の関数を分割 （＊まだラベル位置は不適切）

--- app/src/main/java/com/example/doubleroulette/PieChartView.kt
+++ app/src/main/java/com/example/doubleroulette/PieChartView.kt
@@ -12,7 +12,7 @@ class PieChartView(context: Context, private var rouletteData: RealmResults<Doub
                    private val radius: Float, private val isInner: Boolean
 ) : View(context, null) {
 
-    // ルーレットセル一つ分の角度
+    // ルーレットセル一つ分の角度（＊ラジアンではないことに注意）
     private val partAngle = 360F / rouletteData.count()
     private val paint = Paint()
 
@@ -20,28 +20,33 @@ class PieChartView(context: Context, private var rouletteData: RealmResults<Doub
         super.onDraw(canvas)
         canvas?.let {
             drawRoulette(it)
+            drawRouletteLabel(it)
         }
     }
 
     private fun drawRoulette(canvas: Canvas) {
         var endAngle = 360F
         val rect = RectF(0F, 0F, radius * 2, radius * 2)
-        // ラベルのX座標：内側の場合は radius/2 の位置、外側の場合は radius * 7 / 4 の位置
-        val labelX = if (isInner) radius / 2 else radius * 7 / 4
-        val labelY = radius
         rouletteData.forEach {
-            // ルーレットセルの描画
             paint.color = Color.parseColor("#${it.itemColorR}${it.itemColorG}${it.itemColorB}")
             canvas.drawArc(rect, 0F, endAngle, true, paint)
             endAngle -= partAngle
+        }
+    }
 
-            // TODO: ラベル描画周りが不適切、要修正
-            // ルーレットラベルの追加
-            paint.style = Paint.Style.FILL_AND_STROKE
-            paint.color = Color.BLACK
-            paint.textSize = 40F
-            val labelPoint = CalculateHelper().rotatePoint(labelX, labelY, radius, radius, endAngle + partAngle / 2)
-            canvas.drawText(it.itemName, labelPoint.first(), labelPoint.last(), paint)
+    private fun drawRouletteLabel(canvas: Canvas) {
+        paint.style = Paint.Style.FILL_AND_STROKE
+        paint.color = Color.BLACK
+        paint.textSize = 40F
+        // ラベルのX座標：内側の場合は radius/2 の位置、外側の場合は radius * 7 / 4 の位置
+        val labelX = if (isInner) radius / 2 else radius * 7 / 4
+        val labelY = radius
+        var radian = CalculateHelper().radianFromDegree(partAngle / 2)
+        rouletteData.forEach {
+            val labelPoint =
+                CalculateHelper().rotatePoint(labelX, labelY, radius, radius, radian)
+            canvas.drawText(it.itemName, labelPoint.first() - radius / 8/*ラベルを若干左にずらすため-rad/8*/, labelPoint.last(), paint)
+            radian += CalculateHelper().radianFromDegree(partAngle)
         }
     }
 
