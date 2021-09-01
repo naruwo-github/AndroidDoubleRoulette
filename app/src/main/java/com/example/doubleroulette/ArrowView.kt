4378e368b02b4146aa744b1ce5cf5fd3875dda48
野川成己 <naruo0711@ab.auone-net.jp>
2021-09-01 19:04:43 +0900

    針描画用のフラグメントの追加（描画はまだ）

---
+++ app/src/main/java/com/example/doubleroulette/ArrowView.kt
@@ -0,0 +1,13 @@
+package com.example.doubleroulette
+
+import android.content.Context
+import android.graphics.Canvas
+import android.view.View
+
+class ArrowView(context: Context) : View(context) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+}
